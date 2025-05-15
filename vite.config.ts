8a873238c72d81ab8a305d98589beef41c521bae
Andi Prapugicu <prapugicu.andi02@yahoo.com>
2025-05-15 20:19:12 +0300

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Backend-ul NestJS
+        target: "https://imlazytoreadallat-backend.onrender.com/", // Backend-ul NestJS
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
